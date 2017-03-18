e49bd84723b49fdde7cd228b64476a4149e3373d
Lucas Song <yuanzheng@cs.byu.edu>
2017-03-17 21:50:27 -0600

    added word count

---
+++ example/src/main/scala/example/WordCount.scala
@@ -0,0 +1,21 @@
+/**
+  * Created by ysong on 3/16/17.
+  */
+package example
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/Users/ysong/Documents/OneDrive/workspace/wikipedia.dat" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Word Count Application")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 4).persist()
+    val numAs = logData.filter(line => line.contains("scala")).count()
+    val numBs = logData.filter(line => line.contains("java")).count()
+    println(s"Lines with Scala: $numAs, Lines with Java: $numBs")
+    sc.stop()
+  }
+}
