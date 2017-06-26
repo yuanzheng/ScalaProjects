02f9c4c8fe639e58c2aa23294b0ac301353b118d
Lucas Song <yuanzheng@cs.byu.edu>
2017-06-25 21:58:45 -0600

    finished the Terrain, blocks, moves and neighbors

--- src/main/scala/streams/StringParserTerrain.scala
+++ src/main/scala/streams/StringParserTerrain.scala
@@ -2,6 +2,8 @@ package streams
 
 import common._
 
+import scala.util.{Failure, Success, Try}
+
 /**
  * This component implements a parser to define terrains from a
  * graphical ASCII representation.
@@ -52,7 +54,14 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    case Pos(x, y) => {
+      Try(levelVector(x)(y) != '-') match {
+        case Success(v: Boolean) => v
+        case Failure(e) => false
+      }
+    }
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +71,13 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+
+    val row: Int = levelVector.indexWhere(v => v.indexOf(c) >= 0)
+    val col: Int = levelVector(row).indexOf(c)
+
+    Pos(row, col)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
